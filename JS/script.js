e25b41be78f908629fb5fa8d9662b2bfbf0753a3
ny-Sopheaktra <60089537+ny-Sopheaktra@users.noreply.github.com>
2023-07-26 15:00:32 +0700

    Finish responsive

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,8 @@
 // JavaScript file 
+// toggle 
+const menutoggle = document.querySelector('.toggle');
+const nav = document.querySelector('.nav');
+menutoggle.onclick = function () {
+  menutoggle.classList.toggle('active');
+  nav.classList.toggle('active');
+}
